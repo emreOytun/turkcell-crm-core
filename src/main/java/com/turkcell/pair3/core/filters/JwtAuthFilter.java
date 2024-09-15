a3ef7ea1eb10ffcb3c8b86335947e0fe370d9dc7
emreOytun <emreoytun58@gmail.com>
2024-09-15 16:25:40 +0300

    auth filter logs added

--- src/main/java/com/turkcell/pair3/core/filters/JwtAuthFilter.java
+++ src/main/java/com/turkcell/pair3/core/filters/JwtAuthFilter.java
@@ -4,6 +4,8 @@ import com.turkcell.pair3.core.jwt.JwtService;
 import lombok.RequiredArgsConstructor;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -20,34 +22,50 @@ import java.util.List;
 @RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
+    private final Logger logger = LogManager.getLogger(JwtAuthFilter.class);
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        String jwtHeader = request.getHeader("Authorization");
+        try {
+            String logMessage = null;
+            String jwtHeader = request.getHeader("Authorization");
 
-        if(jwtHeader != null && jwtHeader.startsWith("Bearer "))
-        {
-            String jwt = jwtHeader.substring(7);
+            if (validateJwtHeader(jwtHeader)) {
+                String jwt = jwtHeader.substring(7);
+                String username = jwtService.extractUsername(jwt);
+                List<String> roles = jwtService.extractRoles(jwt);
+                List<SimpleGrantedAuthority> authorities = roles.stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .toList();
 
-            String username = jwtService.extractUsername(jwt);
-
-            List<String> roles = jwtService.extractRoles(jwt);
-
-            List<SimpleGrantedAuthority> authorities = roles.stream()
-                    .map(SimpleGrantedAuthority::new)
-                    .toList();
-
-            if(jwtService.validateToken(jwt, username))
-            {
                 UsernamePasswordAuthenticationToken token = new
                         UsernamePasswordAuthenticationToken(username, null, authorities);
                 token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
                 SecurityContextHolder.getContext().setAuthentication(token);
             }
+
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            logger.error(e, e);
+            logger.error("Request cannot be authenticated. exception: " + e);
         }
-        filterChain.doFilter(request,response);
+    }
+
+    private boolean validateJwtHeader(String jwtHeader) {
+        if (jwtHeader == null || !jwtHeader.startsWith("Bearer ")) {
+            logger.info("JWT Token is null or it does not start with Bearer. JwtHeader: " + jwtHeader);
+            return false;
+        }
+
+        String jwt = jwtHeader.substring(7);
+        String username = jwtService.extractUsername(jwt);
+
+        if (!jwtService.validateToken(jwt, username)) {
+            logger.info("JWT Token cannot be validated. Token: " + jwt);
+            return false;
+        }
+        return true;
     }
 }
